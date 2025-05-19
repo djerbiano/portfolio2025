2160731fdcf722d47464f8fd40d9f7c485b51aaa
djerbiano <saberghoudi2222@hotmail.fr>
2025-05-19 23:26:47 +0200

    Web Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata = {
   title: "GHOUDI Saber | Portfolio",
-  description: "Portfolio de GHOUDI Saber, développeur web basé à Brest, spécialisé en MERN Stack, Java et Spring Boot. Découvrez ses projets, compétences et parcours professionnel.",
+  description:
+    "Portfolio de GHOUDI Saber, développeur web basé à Brest, spécialisé en MERN Stack, Java et Spring Boot. Découvrez ses projets, compétences et parcours professionnel.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
