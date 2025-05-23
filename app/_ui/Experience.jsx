441f171cb60dc846d73e72a1b411e9ac10c428ef
djerbiano <saberghoudi2222@hotmail.fr>
2025-05-23 02:38:12 +0200

    update experience component, fix sync lockfile with package.json

--- app/_ui/Experience.jsx
+++ app/_ui/Experience.jsx
@@ -1,58 +1,76 @@
 "use client";
+import styles from "../page.module.css";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { MdSchool, MdWork } from "react-icons/md";
+import { FaRegStar } from "react-icons/fa";
 export default function Experience() {
+  const observerProps = {
+    triggerOnce: false,
+  };
   return (
-    <VerticalTimeline
-    >
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-        date="2011 - present"
-        iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
-        icon={<span style={{ fontSize: "1.5rem" }}>💼</span>}
-      >
-        <h3 className="vertical-timeline-element-title">poste</h3>
-        <h4 className="vertical-timeline-element-subtitle">zone</h4>
-        <p>tâches</p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-        date="2011 - present"
-        iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
-        icon={<span style={{ fontSize: "1.5rem" }}>💼</span>}
-      >
-        <h3 className="vertical-timeline-element-title">poste</h3>
-        <h4 className="vertical-timeline-element-subtitle">zone</h4>
-        <p>tâches</p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-        date="2011 - present"
-        iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
-        icon={<span style={{ fontSize: "1.5rem" }}>💼</span>}
-      >
-        <h3 className="vertical-timeline-element-title">poste</h3>
-        <h4 className="vertical-timeline-element-subtitle">zone</h4>
-        <p>tâches</p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
-        contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
-        date="2011 - present"
-        iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
-        icon={<span style={{ fontSize: "1.5rem" }}>💼</span>}
-      >
-        <h3 className="vertical-timeline-element-title">poste</h3>
-        <h4 className="vertical-timeline-element-subtitle">zone</h4>
-        <p>tâches</p>
-      </VerticalTimelineElement>
-    </VerticalTimeline>
+    <div className={styles.containerExperience}>
+      <p className={styles.title}>Expérience</p>
+      <VerticalTimeline className={styles.verticalTimeline}>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
+          date="2011 - present"
+          iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
+          icon={<MdSchool color="#000000" />}
+          intersectionObserverProps={observerProps}
+        >
+          <h3 className="vertical-timeline-element-title">poste</h3>
+          <h4 className="vertical-timeline-element-subtitle">zone</h4>
+          <p>tâches</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
+          date="2011 - present"
+          iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
+          icon={<MdSchool color="#000000" />}
+          intersectionObserverProps={observerProps}
+        >
+          <h3 className="vertical-timeline-element-title">poste</h3>
+          <h4 className="vertical-timeline-element-subtitle">zone</h4>
+          <p>tâches</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
+          date="2011 - present"
+          iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
+          icon={<MdWork color="#000000" />}
+          intersectionObserverProps={observerProps}
+        >
+          <h3 className="vertical-timeline-element-title">poste</h3>
+          <h4 className="vertical-timeline-element-subtitle">zone</h4>
+          <p>tâches</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(255, 255, 255)", color: "#000000" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(255, 255, 255)" }}
+          date="2011 - present"
+          iconStyle={{ background: "rgb(255, 255, 255)", color: "#fff" }}
+          icon={<MdWork color="#000000" />}
+          intersectionObserverProps={observerProps}
+        >
+          <h3 className="vertical-timeline-element-title">poste</h3>
+          <h4 className="vertical-timeline-element-subtitle">zone</h4>
+          <p>tâches</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+          icon={<FaRegStar color="#ffffff" />}
+          contentStyle={{ display: "none" }}
+          intersectionObserverProps={observerProps}
+        />
+      </VerticalTimeline>
+    </div>
   );
 }
