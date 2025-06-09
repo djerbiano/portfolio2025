d0c50c1b0c49256f0c3d7bded122d447659c3f2e
djerbiano <saberghoudi2222@hotmail.fr>
2025-06-10 00:35:40 +0200

    add SocialLinks component

--- app/_ui/Footer.jsx
+++ app/_ui/Footer.jsx
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import styles from "../page.module.css";
+import SocialLinks from "./SocialLinks";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()} Saber GHOUDI. Tous droits réservés.</p>
+      <div className={styles.socialLinksContainer}>
+        <SocialLinks />
+      </div>
       <Link href="/mentions-legales" className={styles.btnMentionsLegales}>
         Mentions Légales
       </Link>
+      <p>&copy; {new Date().getFullYear()} Saber GHOUDI. Tous droits réservés.</p>
     </footer>
   );
 }
---
+++ app/_ui/SocialLinks.jsx
@@ -0,0 +1,26 @@
+"use client";
+import styles from "../page.module.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+export default function SocialLinks() {
+  return (
+    <div className={styles.socialLinks}>
+      <a
+        href="https://www.linkedin.com/in/saber-g-ab6251207"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="LinkedIn"
+      >
+        <FaLinkedin />
+      </a>
+      <a
+        href="https://github.com/djerbiano"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="GitHub"
+      >
+        <FaGithub />
+      </a>
+    </div>
+  );
+}
